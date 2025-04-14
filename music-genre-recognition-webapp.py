dc91d8b78dc88f33929ac7f7f3ced934b59e5f94
David-FPI <156213645+David-FPI@users.noreply.github.com>
2025-04-14 23:58:57 +0700

    Update music-genre-recognition-webapp.py

--- music-genre-recognition-webapp.py
+++ music-genre-recognition-webapp.py
@@ -1,5 +1,6 @@
 import streamlit as st
 st.set_page_config(page_title="Music AI Website", layout="wide")
+from streamlit_toggle import st_toggle_switch
 import os
 import bcrypt
 import re  # Thêm thư viện kiểm tra email hợp lệ
